c913f894c2d2388610e617fbb9f53e0979c35fe0
Elwina <elwina_vardal@outlook.com>
2023-08-05 15:38:24 +0800

    New Version 0.8.4.

--- .auto/version.js
+++ .auto/version.js
@@ -1,6 +1,6 @@
-const { write, writeFile } = require('fs');
+const { write, writeFile } = require('fs/promises');
 const { readFile } = require('fs/promises');
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 
 const args = process.argv.slice(2);
 if (args.length !== 1) throw new Error('Invalid number of arguments');
@@ -12,20 +12,20 @@ fileList = [
   './release/app/package-lock.json',
 ];
 
-fileList.forEach((file) => {
-  readFile(file, 'utf8')
-    .then((data) => JSON.parse(data))
-    .then((data) => {
-      const { version } = data;
-      data.version = newVersion;
-      writeFile(file, JSON.stringify(data, null, 2), 'utf8', (err) => {
-        if (err) throw err;
-        console.log(`Updated ${file} version from ${version} to ${newVersion}`);
+async function main() {
+  for (const file of fileList) {
+    let data = await readFile(file, 'utf8');
+    data = await JSON.parse(data);
 
-        exec(`git add *; git commit -m "New Version ${newVersion}.";`);
-      });
-    })
-    .catch((err) => {
-      if (err) throw err;
-    });
-});
+    const { version } = data;
+    data.version = newVersion;
+    await writeFile(file, JSON.stringify(data, null, 2), 'utf8');
+    console.log(`Updated ${file} version from ${version} to ${newVersion}`);
+  }
+
+  execSync(`git add *`);
+  execSync(`git commit -m "New Version ${newVersion}."`);
+  console.log(`Committed changes.`);
+}
+
+main();
