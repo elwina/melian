871d1687fba352c6cf9dab3ed6d54348f671ab1c
Elwina <elwina_vardal@outlook.com>
2024-08-20 23:27:06 +0800

    change config2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: "https://elwina.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: " /melian/",
+  baseUrl: "/melian/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
